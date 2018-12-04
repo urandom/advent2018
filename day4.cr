984058906b49d9740a206842d5af996716f0e5cd
Viktor Kojouharov <developer@sugr.org>
2018-12-04 12:58:42 +0200

    Minor cleanups to day4

--- day4.cr
+++ day4.cr
@@ -33,15 +33,15 @@ lines.sort.each do |line|
 	end
 end
 
-guards = shifts.group_by { |s| s.id }
+guards = shifts.group_by &.id
 sorted_guards = guards.map do |id, shifts|
 	{id, shifts.map { |s| s.awake.count { |v| !v } }.sum}
 end
 
-max_awake = sorted_guards.max_by { |v| v[1] }
+max_awake = sorted_guards.max_by &.[1]
 puts "Most sleepy guard #{max_awake[0]} with minutes #{max_awake[1]}"
 
-shifts = guards[max_awake[0]].map{ |s| s.awake}
+shifts = guards[max_awake[0]].map &.awake
 max_minute = {0, 0}
 0.upto(shifts[0].size-1).each do |m|
 	c = shifts.map { |s| s[m] }.count { |v| !v }
@@ -54,10 +54,10 @@ puts "Max sleepy minute #{max_minute[0]}"
 puts "Part 1 answer: #{max_awake[0] * max_minute[0]}"
 
 max_awake_min = guards.map do |id, shifts|
-	awakes = shifts.map { |s| s.awake }
+	awakes = shifts.map &.awake
 	to_max = awakes.map { |awake| awake.map { |v| v ? 0 : 1 } }.transpose.map { |a| a.sum }
 
-	{id, (to_max.index to_max.max) || 0, awakes.size}
+	{id, (to_max.index to_max.max).not_nil!, awakes.size}
 end
 
 most_sleepy = max_awake_min.sort { |a, b| b[2] <=> a[2] }[0]
