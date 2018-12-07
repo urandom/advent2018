6e54e7feef2de44e9b0626eb6a53d6e6e9a6a4fd
Viktor Kojouharov <developer@sugr.org>
2018-12-07 19:35:14 +0200

    Day 7 part 2

--- day7.cr
+++ day7.cr
@@ -41,4 +41,46 @@ while available.size > 0
 	available = available.to_a.sort
 end
 
+def char_sec(c : Char) : Int32
+	return 60 + c.ord - 64
+end
+
 puts "Directions #{chars.join("")}"
+
+available = children.select { |key| !parents[key]? }.keys
+max_workers = 5
+workers = Hash(Char, Int32).new
+sec = 0
+done = Set(Char).new
+
+while true
+	workers.each do |c, s|
+		if sec == s
+			workers.delete(c)
+			done.add c
+
+			children[c]?.try do |children|
+				children.map { |c| {c, parents[c] } }.each do |c|
+					if c[1].select { |p| !done.includes? p }.size == 0
+						available.push c[0]
+					end
+				end
+			end
+		end
+	end
+
+	available.each do |a|
+		if !workers.has_key?(a) && workers.size < max_workers
+			workers[a] = char_sec(a) + sec
+		end
+	end
+
+	workers.keys.each do |k|
+		available.delete k
+	end
+
+	break if available.size == 0 && workers.size == 0
+	sec+=1
+end
+
+puts "Seconds: #{sec}"
