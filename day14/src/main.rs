3c4030b46c5848ed52c8c32d12d308ff96aa4ee3
Viktor Kojouharov <developer@sugr.org>
2018-12-14 11:04:59 +0200

    Day 14 part 1

---
+++ day14/src/main.rs
@@ -0,0 +1,38 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::io::Result;
+
+fn main() -> Result<()> {
+    let mut reader = BufReader::new(File::open(std::env::args().nth(1).unwrap())?);
+    let mut line = String::new();
+
+    reader.read_line(&mut line)?;
+    let upto : usize = line.trim().parse().unwrap();
+    let total = upto + 10;
+    let mut current = [0, 1];
+    let mut scores = vec![3, 7];
+
+    while scores.len() < total {
+        let sum = scores[current[0]] + scores[current[1]];
+        if sum > 9 {
+            scores.push(sum / 10);
+
+            if scores.len() == total {
+                break;
+            }
+        }
+        scores.push(sum % 10);
+
+        current[0] = (current[0] + scores[current[0]] + 1) % scores.len();
+        current[1] = (current[1] + scores[current[1]] + 1) % scores.len();
+    }
+
+    let mut res = String::new();
+    for i in scores.len()-10..scores.len() {
+        res.push_str((scores[i] as u32).to_string().as_str());
+    }
+    println!("Last 10 scores: {}", res);
+
+    Ok(())
+}
