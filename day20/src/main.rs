89d2173773d3c4af4d5e5471816eacaf166f6634
Viktor Kojouharov <developer@sugr.org>
2018-12-20 16:07:24 +0200

    WIP Day 20 part 2

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,8 +1,9 @@
 use std::fmt;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Result};
+use std::collections::HashMap;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Dir {
     N, E, S, W
 }
@@ -28,9 +29,18 @@ impl Dir {
             _ => None
         }
     }
+
+    fn inc(&self, pos: Pos) -> Pos {
+        match self {
+            Dir::N => (pos.0, pos.1-1),
+            Dir::E => (pos.0+1, pos.1),
+            Dir::S => (pos.0, pos.1+1),
+            Dir::W => (pos.0-1, pos.1),
+        }
+    }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Step {
     Dir(Dir),
     Branches(Vec<Vec<Step>>, bool),
@@ -51,16 +61,19 @@ impl Step {
 #[derive(Debug)]
 struct Path {
     steps: Vec<Step>,
+    debug: bool,
 }
 
+type Pos = (i32, i32);
+
 impl Path {
-    fn new() -> Self {
-        Path{steps: Vec::new()}
+    fn new(debug: bool) -> Self {
+        Path{steps: Vec::new(), debug}
     }
 
-    fn from(input: &str) -> Result<Self> {
+    fn from(input: &str, debug: bool) -> Result<Self> {
         let reader = BufReader::new(File::open(input)?);
-        let mut root = Path::new();
+        let mut root = Path::new(debug);
 
         let line = reader.lines().next().unwrap().unwrap();
         let chars = line.chars();
@@ -86,7 +99,7 @@ impl Path {
 
 
                 loop {
-                    let mut subpath = Path::new();
+                    let mut subpath = Path::new(self.debug);
                     let c = subpath.parse_char_iter(iter);
 
                     if subpath.max_len() == 0 {
@@ -108,15 +121,61 @@ impl Path {
 
         '$'
     }
+
+    fn distances(&self) -> HashMap<Pos, usize> {
+        let mut distances = HashMap::new();
+
+        self.calc_distances((0, 0), 0, &vec![self.steps.clone()], &mut distances);
+        if self.debug {
+            let keys: Vec<&Pos> = distances.keys().collect();
+            let mut sorted: Vec<(Pos, usize)> = Vec::new();
+            for k in keys {
+                sorted.push((*k, distances[k]));
+            }
+            sorted.sort_by_key(|s| s.1);
+            println!("{:?}", sorted);
+        }
+
+        distances
+    }
+
+    fn calc_distances(&self, original_pos: Pos, distance: usize, branches: &Vec<Vec<Step>>, distances: &mut HashMap<Pos, usize>) -> usize {
+        let mut distance = distance;
+
+        for steps in branches {
+            let mut pos = original_pos;
+            for s in steps {
+                match s {
+                    Step::Dir(d) => {
+                        pos = d.inc(pos);
+                        distances.insert(pos, distance + 1);
+                        distance += 1;
+                    },
+                    Step::Branches(branches, false) => {
+                        distance = self.calc_distances(pos, distance, branches, distances);
+                    },
+                    Step::Branches(branches, true) => {
+                        let d = self.calc_distances(pos, distance, branches, distances);
+                        distance += (d - distance) / 2;
+                    },
+                }
+            }
+        }
+
+        distance
+    }
 }
 
 fn main() -> Result<()> {
-    assert_eq!(Path::from("test1.input")?.max_len(), 10);
-    assert_eq!(Path::from("test2.input")?.max_len(), 18);
-    assert_eq!(Path::from("test3.input")?.max_len(), 23);
-    assert_eq!(Path::from("test4.input")?.max_len(), 31);
+    assert_eq!(Path::from("test1.input", false)?.max_len(), 10);
+    assert_eq!(Path::from("test2.input", false)?.max_len(), 18);
+    assert_eq!(Path::from("test3.input", false)?.max_len(), 23);
+    assert_eq!(Path::from("test4.input", false)?.max_len(), 31);
+    assert_eq!(Path::from("test3.input", false)?.distances().iter().filter(|d| *d.1 > 10).count(), 25);
+    assert_eq!(Path::from("test4.input", false)?.distances().iter().filter(|d| *d.1 > 10).count(), 38);
 
-    println!("# of doors: {}", Path::from("input")?.max_len());
+    println!("Farthest door: {}", Path::from("input", false)?.max_len());
+    println!("# of doors: {}", Path::from("input", true)?.distances().iter().filter(|d| *d.1 > 1000).count());
 
     Ok(())
 }
